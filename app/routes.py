7204b5ce15ff47be30a479d541272fd6eaf99bb4
Jean-Mark Wright <jeanmark.wright@gmail.com>
2018-04-02 06:51:27 -0500

    Refactored the template route so it uses a get_template function. Also added a download page.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 import os
 
-from flask import render_template
+from flask import render_template, make_response
+
+import pdfkit
 
 from app import app
 
@@ -22,8 +24,7 @@ def index():
     return render_template("index.html", templates=templates)
 
 
-@app.route('/template/<string:template_name>')
-def template(template_name):
+def get_template(template_name=None, **kwargs):
     filename = "{}/{}.html".format(TEMPLATE_DIR, template_name)
     education_records = Education.query.order_by('-end_date')
     companies = Company.query.all()
@@ -40,7 +41,53 @@ def template(template_name):
         "contact": contact,
         "skills": skills,
         "employments": employments,
+        "include_css": True,
+        "template_name": template_name,
+        "download_mode": False
     }
+    context.update(kwargs)
     if os.path.exists(filename):
         template_name = "resume_templates/{}.html".format(template_name)
         return render_template(template_name, **context)
+    return ""
+
+@app.route('/template/<string:template_name>')
+def template(template_name):
+    return get_template(template_name=template_name)
+
+
+@app.route('/template/<string:template_name>/download')
+def download(template_name):
+    padding = 0.4
+    wkhtml_options = {
+        'margin-top': '{}in'.format(padding),
+        'margin-right': '{}in'.format(padding),
+        'margin-bottom': '{}in'.format(padding),
+        'margin-left': '{}in'.format(padding),
+        'encoding': "UTF-8",
+        'no-outline': None,
+        'page-size': 'A4',
+        'dpi': 400
+    }
+    static_dir = os.path.join(
+        os.path.abspath(os.path.dirname(".")), "app", "static")
+    css = [
+        '/css/{}.css'.format(template_name),
+        '/css/{}-fonts.css'.format(template_name),
+        '/css/font-awesome.min.css',
+        '/css/bootstrap.min.css',
+        '/css/print.css',
+    ]
+    css = ["{}{}".format(static_dir, link) for link in css]
+    template = get_template(
+        template_name=template_name,
+        include_css=False,
+        download_mode=True)
+    filename = "resume_{}.pdf".format(template_name)
+    pdf = pdfkit.from_string(template, False, options=wkhtml_options, css=css)
+    response = make_response(pdf)
+    response.headers['Content-Disposition'] = 'attachment; filename={}'.format(
+            filename)
+    response.mimetype = 'application/pdf'
+    return response
+
