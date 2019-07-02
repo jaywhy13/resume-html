191e7c59e741be3aa90a8b03a846099deda2a6d7
Jean-Mark Wright <jeanmark.wright@gmail.com>
2019-07-02 07:09:30 -0400

    Added Framework to admin

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,20 @@
 from flask import Flask
-from .config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+
+from .config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-admin = Admin(app, name='resume', template_mode='bootstrap3')
+admin = Admin(app, name="resume", template_mode="bootstrap3")
 
-from app import routes, models
+from app import models, routes
 
 admin.add_view(ModelView(models.School, db.session))
 admin.add_view(ModelView(models.Education, db.session))
@@ -22,3 +24,4 @@ admin.add_view(ModelView(models.Project, db.session))
 admin.add_view(ModelView(models.Technology, db.session))
 admin.add_view(ModelView(models.Contact, db.session))
 admin.add_view(ModelView(models.Skill, db.session))
+admin.add_view(ModelView(models.Framework, db.session))
